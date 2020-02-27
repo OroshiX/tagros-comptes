fd8677ec3078113f4dccd042941ef195c5557ad4
Jessica <OroshiX@users.noreply.github.com>
2020-02-27 18:11:25 +0100

    cleanup database with constants

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -12,6 +12,25 @@ class DBProvider {
   DBProvider._();
 
   static final DBProvider db = DBProvider._();
+  static final String entryTable = 'infoEntry';
+  static final String gameTable = 'game';
+  static final String playerTable = 'player';
+
+  static final String id = 'id';
+
+  // game fields
+  static final String nbPlayers = 'nbPlayers';
+
+  // Player fields
+  static final String name = 'name';
+
+  // infoEntry fields
+  static final String player = 'player';
+  static final String prise = 'prise';
+  static final String points = 'points';
+  static final String game = 'game';
+  static final String nbBouts = 'nbBouts';
+
   Database _database;
 
   Future<Database> get database async {
@@ -32,27 +51,42 @@ class DBProvider {
     return await openDatabase(path,
         version: DATABASE_VERSION,
         onOpen: (db) async {}, onCreate: (Database db, int version) async {
-      // Create the entry table
-      await db.execute('''
-          CREATE TABLE infoEntry(
-            id INTEGER PRIMARY KEY,
-            player TEXT NOT NULL,
-            prise TEXT NOT NULL,
-            points REAL,
-            nbBouts INTEGER
+          // TODO create the game table
+          await db.execute('''
+          CREATE TABLE $gameTable(
+            $id INTEGER PRIMARY KEY,
+            $nbPlayers INTEGER
+          )
+          ''');
+
+          await db.execute('''
+          CREATE TABLE $playerTable(
+            $id INTEGER PRIMARY KEY,
+            $name TEXT NOT NULL
+          )''');
+
+          // Create the entry table
+          await db.execute('''
+          CREATE TABLE $entryTable(
+            $id INTEGER PRIMARY KEY,
+            $player TEXT NOT NULL,
+            $prise TEXT NOT NULL,
+            $points REAL,
+            $game INTEGER,
+            $nbBouts INTEGER
           )''');
     });
   }
 
   Future<int> newEntry(InfoEntry infoEntry) async {
     final db = await database;
-    var res = await db.insert('infoEntry', infoEntry.toJson());
+    var res = await db.insert(entryTable, infoEntry.toJson());
     return res;
   }
 
   Future<List<InfoEntry>> getInfoEntries() async {
     final db = await database;
-    var res = await db.query('infoEntry');
+    var res = await db.query(entryTable);
     List<InfoEntry> entries = res.isNotEmpty
         ? res.map((entry) => InfoEntry.fromJson(entry)).toList()
         : [];
@@ -61,20 +95,20 @@ class DBProvider {
 
   Future<InfoEntry> getEntry(int id) async {
     final db = await database;
-    var res = await db.query('infoEntry', where: 'id = ?', whereArgs: [id]);
+    var res = await db.query(entryTable, where: '$id = ?', whereArgs: [id]);
     return res.isNotEmpty ? InfoEntry.fromJson(res.first) : null;
   }
 
   Future<int> updateEntry(InfoEntry entry) async {
     final db = await database;
-    var res = await db.update('infoEntry', entry.toJson(),
+    var res = await db.update(entryTable, entry.toJson(),
         where: 'id = ?', whereArgs: [entry.id]);
     return res;
   }
 
   Future<int> deleteEntry(int id) async {
     final db = await database;
-    var res = await db.delete('infoEntry', where: 'id = ?', whereArgs: [id]);
+    var res = await db.delete(entryTable, where: '$id = ?', whereArgs: [id]);
     return res;
   }
 }
